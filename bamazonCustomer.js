f998fb3f02418be2e96b397bfb1ce54e0b9c0715
akayne <15270231+arielkayne@users.noreply.github.com>
2017-12-14 21:50:01 -0600

    inventory check works, inventory update works, logic for fulful the order works, order price is displayed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,12 @@
 var fs = require("fs");
 // Require the NPM Package inquirer
 var inquirer = require("inquirer");
-
 //require the musql package
 var mysql = require("mysql");
+
+// global variables just because...IDK better ways
+var buyIt;
+var thisQuant;
 
 // pull up and display inventory(id, name, price)
 var connection = mysql.createConnection({
@@ -51,7 +54,7 @@ function askUser(){
 	  	}
 	]).then(function(selection){
 		console.log("You selected: "+selection.buyThis);
-		//close out the database connection
+		buyIt = selection.buyThis;
 		howMany();
 	})
 }
@@ -66,16 +69,32 @@ function howMany(){
 	  	}
 	]).then(function(selection){
 		console.log("You ordered: "+selection.thisMany);
-		//close out the database connection
-		connection.end();
+		thisQuant = selection.thisMany;
+		checkInventory();
 	})
 }
 
 // check to see if inventory available...
-
-
-// 	if not...insufficient quantity messages
-
-// 	if yes...fulfil the customer order
-// 		decrement remaining quantity
-// 		display the customer total order cost
+function checkInventory(){
+	console.log("Confirming availability...\n");
+	connection.query("SELECT * FROM products WHERE item_id="+buyIt, function(err, res) {
+		if (err) throw err;
+		// 	if not...insufficient quantity messages
+		if (res[0].stock_quantity<=thisQuant){
+			console.log("Your order could not be fulfilled because we don't have enough of '"+res[0].product_name+"' available.");
+		}
+		// 	if yes...fulfil the customer order
+		else {
+			//decrements remaining quantity
+			connection.query("UPDATE products SET stock_quantity="+(res[0].stock_quantity-thisQuant)+" WHERE item_id="+buyIt);
+			//display the customer total order cost
+			console.log("Your order has been fulfilled. You owe $"+(buyIt*thisQuant)+" to Bamazon's receivable department. They will contact you shortly.");
+			connection.query("SELECT * FROM products WHERE item_id="+buyIt, function(err, res){
+				if (err) throw err;
+				console.log("The store now has: "+res[0].stock_quantity+" of "+res[0].product_name+" remaining in inventory.");		
+			});
+		}
+		//close out the database connection
+		connection.end();
+	});
+}
